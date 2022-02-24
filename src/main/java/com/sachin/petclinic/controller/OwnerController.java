9e5753b01db37e3a08c0d95f1b55d60105919954
sachinmewar <sachinmewar800@gmail.com>
2022-02-25 00:13:10 +0530

    Owner index page. Close #11

--- src/main/java/com/sachin/petclinic/controller/OwnerController.java
+++ src/main/java/com/sachin/petclinic/controller/OwnerController.java
@@ -1,14 +1,27 @@
 package com.sachin.petclinic.controller;
 
+import com.sachin.petclinic.model.Owner;
+import com.sachin.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model) {
+        Set<Owner> ownerList = ownerService.findAll();
+        model.addAttribute("owner", ownerList);
         return "owners/index";
     }
 }
