0f60eaf3ccbd59c000876122cf1ef18c0c764888
sachinmewar <sachinmewar800@gmail.com>
2022-02-25 00:23:43 +0530

    Vet index page. Close #12

--- src/main/java/com/sachin/petclinic/controller/VetController.java
+++ src/main/java/com/sachin/petclinic/controller/VetController.java
@@ -1,14 +1,28 @@
 package com.sachin.petclinic.controller;
 
+import com.sachin.petclinic.model.Vet;
+import com.sachin.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"", "/", "/index", "/index.html" })
-    public String listVets(){
+    public String listVets(Model model){
+        Set<Vet> vetList = vetService.findAll();
+        model.addAttribute("vet", vetList);
         return "vets/index";
     }
 }
